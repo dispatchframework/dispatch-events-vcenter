4de87a10d57b9088e55cd0be0e772fd460e821fc
Sabari Kumar Murugesan <smurugesan@vmware.com>
2018-12-19 15:34:41 -0800

    Support for Knative Eventing

--- main.go
+++ main.go
@@ -12,14 +12,29 @@ import (
 var dryRun = flag.Bool("dry-run", false, "Debug, pull messages and do not send Dispatch events")
 var vcenterURL = flag.String("vcenterurl", "", "URL to vCenter instance (i.e. cloudadmin@vmc.local:<password>@vcenter.corp.local:443)")
 var debug = flag.Bool("debug", false, "Enable debug mode (print more information)")
+// TODO: Deprecate gateway flag in favor of sink
+// (optional) gateway flag must be set if this event driver is used with Dispatch Solo. Note: this flag will be
+// ignored when using this event driver with knative container source.
 var dispatchEndpoint = flag.String(driverclient.DispatchEventsGatewayFlag, "localhost:8080", "events api endpoint")
+// (not required) sink will be automatically set when using with knative container source. Note: this flag will supersede
+// gateway.
+var sink = flag.String("sink", "", "knative sink url")
 
 func getDriverClient() driverclient.Client {
 	if *dryRun {
 		return nil
 	}
 	token := os.Getenv(driverclient.AuthToken)
-	client, err := driverclient.NewHTTPClient(driverclient.WithGateway(*dispatchEndpoint), driverclient.WithToken(token))
+	var client driverclient.Client
+	var err error
+	// If sink is set use the sink url and ignore gateway
+	if *sink != "" {
+		log.Printf("Using sink URL %s", *sink)
+		client, err = driverclient.NewHTTPClient(driverclient.WithURL(*sink), driverclient.WithToken(token))
+	} else {
+		client, err = driverclient.NewHTTPClient(driverclient.WithGateway(*dispatchEndpoint), driverclient.WithToken(token))
+	}
+
 	if err != nil {
 		log.Fatalf("Error when creating the events client: %s", err.Error())
 	}
